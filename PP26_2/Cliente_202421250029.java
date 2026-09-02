1e73615dd104038a0effcc7167ae5fe129a74022
DevKafé <140562139+RenatoDevK@users.noreply.github.com>
2026-09-02 08:17:49 -0300

    Add files via upload

---
+++ PP26_2/Cliente_202421250029.java
@@ -0,0 +1,12 @@
+public class Cliente_202421250029 {
+    public static void main(String[] args) {
+        Calculadora_202421250029 calc = new Calculadora_202421250029();
+        System.out.println(calc.somar(10, 5));      
+        System.out.println(calc.dividir(10, 5));
+        System.out.println(calc.subtrair(10, 5));
+        System.out.println(calc.multiplicar(10, 5)); 
+        System.out.println(calc.somar(-10, 5));       // -5.0
+        System.out.println(calc.multiplicar(-2, -3)); // 6.0 
+        System.out.println(calc.dividir(0, 0));       // -5.0
+    }
+}
